bfdefc5acc9fce2174ae8ef1f6ce2db74acc0e81
Vincent Hanquez <vincent@snarc.org>
2010-12-06 22:45:20 +0000

    process system certificate and check for the one we can't decode properly.
    Not yet finish test, and for now not really an unit test. Later one, we can
    use this base to test that encode . decode = data as read, which is useful
    for the signature bit.

--- Tests/Unit.hs
+++ Tests/Unit.hs
@@ -8,6 +8,7 @@ import Control.Monad
 import Control.Applicative ((<$>))
 import Control.Exception
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
 import Data.Certificate.X509
 import Data.Certificate.PEM
 import Data.List (isPrefixOf)
@@ -24,4 +25,17 @@ readAllSystemCertificates = do
 runTests :: IO ()
 runTests = do
 	certs <- readAllSystemCertificates
+	forM certs $ \(name, cert) -> do
+		case decodeCertificate $ L.fromChunks [cert] of
+			Left err -> putStrLn ("cannot decode certificate " ++ name ++ " " ++ show err)
+			Right c  -> do
+				case certSignatureAlg c of
+					SignatureALG_Unknown oid -> putStrLn ("unknown signature algorithm " ++ show oid ++ " decoding " ++ name)
+					_                        -> return ()
+				case certPubKey c of
+					(PubKey (PubKeyALG_Unknown oid) _) -> putStrLn ("unknown public key alg " ++ show (certPubKey c) ++ " decoding " ++ name)
+					(PubKey _ (PubKeyUnknown l))       -> putStrLn ("unknown public key " ++ show (certPubKey c) ++ " decoding " ++ name)
+					(PubKey _ (PubKeyECDSA x))         -> putStrLn ("unknown public ECDSA key " ++ show x ++ " decoding " ++ name)
+					_                                  -> return ()
+				return ()
 	return ()
