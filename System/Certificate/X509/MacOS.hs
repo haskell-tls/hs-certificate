c9079e776588fbcfab77a4c8cc38ecb839ad6dfa
Hiromi Ishii <konn.jinro@gmail.com>
2011-10-07 23:14:51 +0900

    Implemented findCertificate for Mac OS X

--- System/Certificate/X509/MacOS.hs
+++ System/Certificate/X509/MacOS.hs
@@ -4,6 +4,17 @@ module System.Certificate.X509.MacOS
 
 import Data.Certificate.X509
 import Data.Certificate.PEM
+import System.Process
+import Data.ByteString hiding (filter, map)
+import qualified Data.ByteString.Lazy as LBS
+import Control.Applicative
+import Data.Either
+import Data.Maybe
 
 findCertificate :: (X509 -> Bool) -> IO (Maybe X509)
-findCertificate f = undefined
+findCertificate f = do
+  (_, h, _, ph) <- runInteractiveCommand "security find-certificate -pa"
+  waitForProcess ph
+  pems <- parsePEMs <$> hGetContents h
+  let targets = rights $ map (decodeCertificate . LBS.fromChunks .  pure . snd) $ filter ((=="CERTIFICATE") . fst) pems
+  return $ listToMaybe $ filter f targets
