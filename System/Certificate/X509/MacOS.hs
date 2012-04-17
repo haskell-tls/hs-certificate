4a99a69c764d8075d1be7117fbda717c19d41592
Vincent Hanquez <vincent@snarc.org>
2012-04-17 12:27:42 +0100

    Fix build in macos. the macos module is now always built to prevent future compilation problems, and exposed.

--- System/Certificate/X509/MacOS.hs
+++ System/Certificate/X509/MacOS.hs
@@ -2,6 +2,7 @@ module System.Certificate.X509.MacOS
 	( findCertificate
 	) where
 
+import Data.PEM (pemParseLBS, PEM(..))
 import Data.Certificate.X509
 import System.Process
 import Data.ByteString hiding (filter, map)
@@ -16,7 +17,7 @@ keyChain = "/System/Library/Keychains/SystemRootCertificates.keychain"
 findCertificate :: (X509 -> Bool) -> IO (Maybe X509)
 findCertificate f = do
   (_, Just hout, _, ph) <- createProcess (proc "security" ["find-certificate", "-pa", keyChain]) { std_out = CreatePipe }
-  pems <- parsePEMs <$> hGetContents hout
+  pems <- either error id . pemParseLBS <$> LBS.hGetContents hout
   _ <- waitForProcess ph
-  let targets = rights $ map (decodeCertificate . LBS.fromChunks .  pure . snd) $ filter ((=="CERTIFICATE") . fst) pems
+  let targets = rights $ map (decodeCertificate . LBS.fromChunks .  pure . pemContent) $ filter ((=="CERTIFICATE") . pemName) pems
   return $ listToMaybe $ filter f targets
