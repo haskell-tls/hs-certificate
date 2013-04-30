d8205aa718acd3ca73a361e7576d43edd7caed20
Vincent Hanquez <vincent@snarc.org>
2013-04-30 09:14:09 +0100

    add validation module

---
+++ x509-validation/Data/X509/Validation.hs
@@ -0,0 +1,14 @@
+-- |
+-- Module      : Data.X509.Validation
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- X.509 Certificate checks and validations
+--
+-- Follows RFC5280 / RFC6818
+--
+module Data.X509.Validation
+    (
+    ) where
