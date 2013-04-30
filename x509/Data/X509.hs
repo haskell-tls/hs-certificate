6879c5e039b3d2b091f3b0a0f701de09183223a1
Vincent Hanquez <vincent@snarc.org>
2013-04-30 08:30:13 +0100

    fix hash capability

--- x509/Data/X509.hs
+++ x509/Data/X509.hs
@@ -55,10 +55,14 @@ import qualified Crypto.Hash.SHA1 as SHA1
 type SignedCertificate = SignedExact Certificate
 type SignedCRL         = SignedExact CRL
 
--- | Make an openssl style hash of distinguished name
+-- | Make an OpenSSL style hash of distinguished name
+--
+-- OpenSSL algorithm is odd, and has been replicated here somewhat.
+-- only lower the case of ascii character.
 hashDN :: DistinguishedName -> B.ByteString
-hashDN = shorten . SHA1.hash . encodeASN1' DER . Cert.encodeDNinner toLowerUTF8
-    where toLowerUTF8 (_, s) = (UTF8, B.map asciiToLower s)
+hashDN = shorten . SHA1.hash . encodeASN1' DER . flip toASN1 [] . DistinguishedNameInner . dnLowerUTF8
+    where dnLowerUTF8 (DistinguishedName l) = DistinguishedName $ map toLowerUTF8 l
+          toLowerUTF8 (oid, (_, s)) = (oid, (UTF8, B.map asciiToLower s))
           asciiToLower c
             | c >= w8A && c <= w8Z = fromIntegral (fromIntegral c - fromEnum 'A' + fromEnum 'a')
             | otherwise            = c
@@ -67,7 +71,7 @@ hashDN = shorten . SHA1.hash . encodeASN1' DER . Cert.encodeDNinner toLowerUTF8
 
 -- | Create an openssl style old hash of distinguished name
 hashDN_old :: DistinguishedName -> B.ByteString
-hashDN_old = shorten . MD5.hash . encodeASN1' DER . Cert.encodeDN
+hashDN_old = shorten . MD5.hash . encodeASN1' DER . flip toASN1 []
 
 shorten :: B.ByteString -> B.ByteString
 shorten b = B.pack $ map i [3,2,1,0]
