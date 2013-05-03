28a361d3acd0a8c77f30a98e6aed0a8b9ca1b79f
Vincent Hanquez <vincent@snarc.org>
2013-05-03 08:37:48 +0100

    WIP validation

--- x509-validation/Data/X509/Validation.hs
+++ x509-validation/Data/X509/Validation.hs
@@ -26,6 +26,9 @@ data FailedReason =
     | UnknownCA                -- ^ unknown Certificate Authority (CA)
     | NotAllowedToSign         -- ^ certificate is not allowed to sign (not a CA)
     | SignatureFailed          -- ^ signature failed
+    | NoCommonName             -- ^ Certificate doesn't have any common name (CN)
+    | NameMismatch String      -- ^ connection name and certificate do not match
+    | InvalidWildcard          -- ^ invalid wildcard in certificate
     deriving (Show,Eq)
 
 data Checks = Checks
@@ -44,9 +47,63 @@ validate checks certificateChain = do
     doCheck 
   where doCheck = return []
 
+-- | Validate that the current time is between validity bounds
 validateTime :: UTCTime -> Certificate -> [FailedReason]
-validateTime currentTime cert =
+validateTime currentTime cert
     | currentTime < before = [InFuture]
     | currentTime > after  = [Expired]
     | otherwise            = []
   where (before, after) = certValidity cert
+
+getNames :: Certificate -> (Maybe String, [String])
+getNames cert = (commonName, altNames)
+  where commonName = getDnElement DnCommonName $ getSubjectDN cert
+        altNames   = maybe [] (maybe [] toAltName . extensionGet) $ certExtensions cert
+        toAltName (ExtSubjectAltName names) = names
+
+validateCertificateName :: Certificate -> [FailedReason]
+validateCertificateName cert
+    | commonName == Nothing = [NoCommonName]
+    | otherwise             = []
+  where (commonName, altNames) = getNames cert
+
+-- | Validate that the name used to connect to a host
+-- match the certificate used.
+validateNameMatch :: String -> Certificate -> [FailedReason]
+validateNameMatch fqhn cert =
+    let names = maybe [] ((:[]) . snd) (lookup oidCommonName $ getDistinguishedElements $ certSubjectDN cert)
+             ++ maybe [] (maybe [] toAltName . extensionGet) (certExtensions cert) in
+    orUsage $ map (matchDomain . splitDot) names
+    where
+        orUsage [] = undefined--[rejectMisc "FQDN do not match this certificate"
+        orUsage (x:xs)
+            | x == []   = []
+            | otherwise = orUsage xs
+
+        toAltName (ExtSubjectAltName l) = l
+        matchDomain l
+            | length (filter (== "") l) > 0 = rejectMisc "commonname OID got empty subdomain"
+            | head l == "*"                 = wildcardMatch (reverse $ drop 1 l)
+            | otherwise                     = if l == splitDot fqhn
+                then CertificateUsageAccept
+                else rejectMisc "FQDN and common name OID do not match"
+
+
+        -- only 1 wildcard is valid, and if multiples are present
+        -- they won't have a wildcard meaning but will be match as normal star
+        -- character to the fqhn and inevitably will fail.
+        wildcardMatch l
+            -- <star>.com or <star> is always invalid
+            | length l < 2                         = [InvalidWildcard]
+            --
+            | length (head l) <= 2 && length (head $ drop 1 l) <= 3 && length l < 3 = [InvalidWilcard]
+            | otherwise                            =
+                if l == take (length l) (reverse $ splitDot fqhn)
+                    then CertificateUsageAccept
+                    else NameMismatchrejectMisc "FQDN and common name OID do not match"
+
+        splitDot :: String -> [String]
+        splitDot [] = [""]
+        splitDot x  =
+            let (y, z) = break (== '.') x in
+            y : (if z == "" then [] else splitDot $ drop 1 z)
