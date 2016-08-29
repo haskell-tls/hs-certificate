1cda904967cbfd83a1ac1af5a1a6d315d6dd184c
Olivier Chéron <ocheron@users.noreply.github.com>
2016-08-29 09:50:55 +0200

    Checks EC public-key length
    The condition was reversed, and at the same time did not compare the right
    variable.

--- x509-validation/Data/X509/Validation/Signature.hs
+++ x509-validation/Data/X509/Validation/Signature.hs
@@ -138,7 +138,7 @@ verifyECDSA hashALG key =
                 Nothing                -> Nothing
                 Just (ptFormat, input) ->
                     case ptFormat of
-                        4 -> if B.length bs == 2 * bytes
+                        4 -> if B.length input /= 2 * bytes
                                 then Nothing
                                 else
                                     let (x, y) = B.splitAt bytes input
