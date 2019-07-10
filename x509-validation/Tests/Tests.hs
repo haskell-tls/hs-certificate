2a71b5271b83e87a1fd14c53cc5987bef6e07a06
Akshay Mankar <itsakshaymankar@gmail.com>
2019-07-10 07:31:16 +0100

    Use latest version of foundation
    This is required to make sure IPv4 parsing handles overflow correctly.
    See haskell-foundation/foundation#526 for details.

--- x509-validation/Tests/Tests.hs
+++ x509-validation/Tests/Tests.hs
@@ -576,8 +576,7 @@ treeWithAlg groupName alg = withResource (initData alg) freeData $ \res ->
           , testSubjectAltNameIP res (BS.pack [10,0,0,1]) "10.0.0.1" True []
           , testSubjectAltNameIP res (BS.pack [10,0,0,1]) "10.0.0.2" True [NameMismatch "10.0.0.2"]
           , testSubjectAltNameIP res (BS.pack [10,0,0,1]) "10.0.0.1.example.com" True [NameMismatch "10.0.0.1.example.com"]
-          -- This will have to be fixed in parser library, https://github.com/haskell-foundation/foundation/issues/523
-          -- , testSubjectAltNameIP res (BS.pack [10,0,0,1]) "266.0.0.1" True [NameMismatch "266.0.0.1"] -- 266 read in Word8 is 10
+          , testSubjectAltNameIP res (BS.pack [10,0,0,1]) "266.0.0.1" True [NameMismatch "266.0.0.1"] -- 266 read in Word8 is 10, hence this test
 
           , testSubjectAltNameIP res (BS.pack [0x20,0x01,0x0d,0xb8,0x85,0xa3,0,0,0,0,0x8a,0x2e,0x03,0x70,0x73,0x34]) "2001:0db8:85a3:0000:0000:8a2e:0370:7334" True []
           , testSubjectAltNameIP res (BS.pack [0x20,0x01,0x0d,0xb8,0x85,0xa3,0,0,0,0,0x8a,0x2e,0x03,0x70,0x73,0x34]) "2001:0db8:85a3::8a2e:0370:7334" True []
